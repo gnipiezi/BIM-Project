32394fa7dbaf646374735fa15a6deb529c1b8308
GNIPIEZI AURIOL <auriol.gnipiezi@viacesi.fr>
2024-03-27 15:55:49 +0100

    Add handle_excel_data to generate IFC files

--- code.py
+++ code.py
@@ -7,8 +7,12 @@ import time
 import os
 import subprocess
 import logging
-logging.basicConfig(level=logging.INFO)
+
+excel_path = os.path.abspath("isolants.xlsx")
 ifc_path = './Panneau MYRAL M32.Ifc'
+output_folder = './output'  # Dossier de sortie pour les nouveaux fichiers IFC
+
+logging.basicConfig(level=logging.INFO)
 ifc_file = ifcopenshell.open(ifc_path)
 walls = ifc_file.by_type('IfcWall')
 command = 'Rscript'
@@ -58,12 +62,34 @@ class ExcelChangeHandler(FileSystemEventHandler):
         if os.path.abspath(event.src_path) == file_to_watch:
             print("Le fichier Excel a été modifié. Lecture des données...")
             df = pd.read_excel(event.src_path)
-            handle_excel_data(df)
+            handle_excel_data(excel_path, ifc_path, output_folder)
 
-def handle_excel_data(df):
-    for index, row in df.iterrows():
-        pass
-        # Traitement de l'isolant : print(f"Traitement de l'isolant : {row['Nom de l'isolant']}")
+def handle_excel_data(excel_path, ifc_path, output_folder):
+    df_isolants = pd.read_excel(excel_path, usecols=[0], engine='openpyxl')
+    df_isolants = df_isolants.dropna()  
+
+    ifc_file = ifcopenshell.open(ifc_path)
+    walls = ifc_file.by_type('IfcWall')
+
+    for index, row in df_isolants.head(3).iterrows():
+        new_isolant_name = row[df_isolants.columns[0]]
+
+        for wall in walls:
+            material_associations = ifc_file.get_inverse(wall, 'ReferencedBy')
+            for association in material_associations:
+                if association.is_a('IfcRelAssociatesMaterial'):
+                    material_definition = association.RelatingMaterial
+                    if material_definition.is_a('IfcMaterialLayerSetUsage'):
+                        material_layer_set = material_definition.ForLayerSet
+                        for material_layer in material_layer_set.MaterialLayers:
+                            material_layer.Material.Name = new_isolant_name
+
+        new_ifc_filename = f"{new_isolant_name.replace(' ', '_')}.ifc"
+        new_ifc_path = os.path.join(output_folder, new_ifc_filename)
+        ifc_file.write(new_ifc_path)
+        print(f"Le fichier IFC avec le nouvel isolant '{new_isolant_name}' a été sauvegardé sous : {new_ifc_path}")
+
+
 
 folder_path = os.path.dirname(file_to_watch)  
 event_handler = ExcelChangeHandler()
@@ -77,5 +103,4 @@ try:
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
-    observer.stop()
-observer.join()
+    observer.stop() 
